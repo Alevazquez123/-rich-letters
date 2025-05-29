d3a1252a37a8ea84f5d09133e4d1dc1a25cb0f6b
Alevazquez123 <6a.vaba@gmail.com>
2025-05-28 19:48:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -70,3 +70,8 @@ def historial():
 # Crear la base de datos si no existe (cuando Gunicorn inicie)
 with app.app_context():
     db.create_all()
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)
+
